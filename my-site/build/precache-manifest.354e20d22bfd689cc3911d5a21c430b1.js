504be9e2b6ee87eff2b7d3973d7440159d20b6fa
austrisD <austris.daugulis@gmail.com>
2020-09-04 18:23:30 +0300

    changing gitignore

---
+++ my-site/build/precache-manifest.354e20d22bfd689cc3911d5a21c430b1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b97e175ecb5d65cfb6e08b2fb1eaba",
+    "url": "/portfolio/my-site/index.html"
+  },
+  {
+    "revision": "ea66263608cfef5a29cc",
+    "url": "/portfolio/my-site/static/css/main.c14cbb0e.chunk.css"
+  },
+  {
+    "revision": "e56b8bd59c13c73bd300",
+    "url": "/portfolio/my-site/static/js/2.baeddf00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/my-site/static/js/2.baeddf00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea66263608cfef5a29cc",
+    "url": "/portfolio/my-site/static/js/main.2dfdf6ff.chunk.js"
+  },
+  {
+    "revision": "f8d4857949990bc44b5d",
+    "url": "/portfolio/my-site/static/js/runtime-main.e840be78.js"
+  },
+  {
+    "revision": "0a1ee68111416520f9a7020fb409c67f",
+    "url": "/portfolio/my-site/static/media/Avatar.0a1ee681.JPG"
+  },
+  {
+    "revision": "712e5c61b3511b5ad6b78da1359e00ca",
+    "url": "/portfolio/my-site/static/media/Orbitron-Black.712e5c61.ttf"
+  },
+  {
+    "revision": "add0c4244a015960586eeb51f091fd71",
+    "url": "/portfolio/my-site/static/media/Orbitron-Regular.add0c424.ttf"
+  },
+  {
+    "revision": "173deda76bdb9ae0337aa8d71cbf832e",
+    "url": "/portfolio/my-site/static/media/landing_prew.173deda7.jpg"
+  },
+  {
+    "revision": "fd7ce6693d4497ca8d8ba5d0243fd859",
+    "url": "/portfolio/my-site/static/media/login_prew.fd7ce669.jpg"
+  },
+  {
+    "revision": "03fb4d4940e0e3eb4f9299196f5001c2",
+    "url": "/portfolio/my-site/static/media/profile_prew.03fb4d49.jpg"
+  },
+  {
+    "revision": "b0cbad9e1a89335154b3004d795e0773",
+    "url": "/portfolio/my-site/static/media/todolistPrew.b0cbad9e.jpg"
+  },
+  {
+    "revision": "87e52866eff3d6fcfc57428a4db7ebcf",
+    "url": "/portfolio/my-site/static/media/webskola.lv_prew.87e52866.jpg"
+  }
+]);
