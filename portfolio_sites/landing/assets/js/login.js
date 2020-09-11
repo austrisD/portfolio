0f05c0e33ee215074dfe9e4ffd651203107f72f3
austrisD <zark@inbox.lv>
2020-09-11 17:58:47 +0300

    remove_files

--- portfolio_sites/landing/assets/js/login.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  let login = document.querySelector(".login");
-  login.classList.add("appear");
-  setTimeout(() => {
-    login.style.opacity = "100%";
-  }, 2000);
-});
